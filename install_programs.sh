e1d69c4ec5dbdd20ed465d50881846c3611b9ba8
Glassinatorn <gusten1995@gmail.com>
2025-10-07 00:48:18 +0200

    added beginning of script for installing programs

---
+++ install_programs.sh
@@ -0,0 +1,12 @@
+# TODO:
+# - add nvim []
+# - add tmux []
+# - add nnn []
+# - add sol [x]
+brew install --cask sol
+# - add sketchybar []
+# - add yabai []
+# - add sxhkd []
+# - add iterm/alakitty []
+# - add sbar []
+# - add svim []
