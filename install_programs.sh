c505872bb4d25d0760554c745528b9f30d95e24a
axel gehlin björnberg <glass@axels-macbook-pro.tail1040f6.ts.net>
2026-04-19 10:42:28 +0200

    updated install script with the tools I want

--- install_programs.sh
+++ install_programs.sh
@@ -1,12 +1,25 @@
 # TODO:
-# - add nvim []
-# - add tmux []
-# - add nnn []
+# - add lazynvim [x]
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+# - add tmux [x]
+brew install tmux
+# - add nnn [x]
+brew install nnn
+# - add gtop [x]
+brew install gtop
 # - add sol [x]
-brew install --cask sol
-# - add sketchybar []
-# - add yabai []
-# - add sxhkd []
-# - add iterm/alakitty []
-# - add sbar []
-# - add svim []
+brew install sol
+# - add sketchybar [x]
+brew install sketchybar
+brew install font-sketchybar-app-font
+# - add yabai [x]
+brew install yabai
+# - add svim [x]
+brew tap FelixKratz/formulae
+brew install svim
+brew services start svim
+# - add nerd font 5 [x]
+brew search '/font-.*-nerd-font/' | awk '{ print $1 }' | xargs -I{} brew install --cask {} || true
+# add fzf
+brew install fzf
